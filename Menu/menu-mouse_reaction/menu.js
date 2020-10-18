c455ace2fcb4f610c1ce9254a518e1d509c25e45
yulahabogoslavets <speed0435@gmail.com>
2020-10-18 14:05:52 +0200

    menu with reaction on mouse

---
+++ Menu/menu-mouse_reaction/menu.js
@@ -0,0 +1,23 @@
+window.onload = function(){
+
+	document.querySelector('#menu').onmouseover = menuShow;
+	document.querySelector('#menu').onmouseout = menuHide;
+
+	document.onkeydown = function(event){
+		//console.log(event);
+		if (event.code =='KeyM') {
+			menuShow();
+		}
+		if (event.code == 'Escape') {
+			menuHide();
+		}
+	}
+
+	function menuShow(){
+		document.querySelector('#menu').style.left = 0;
+	}
+
+	function menuHide(){
+		document.querySelector('#menu').style.left = '-230px';
+	}
+}
